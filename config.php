382c76fe357e0672c92ccdc7240b7a65091db544
Isla Visual <islavisual@gmail.com>
2015-06-04 17:11:35 +0200

    update 1.01
    Update new features

--- config.php
+++ config.php
@@ -4,16 +4,16 @@
         define('LOCALHOST',true);
         define('DEVELOPMENT_ENVIRONMENT',false);
         define('PRODUCTION_ENVIRONMENT',false);
-        define('DB_USER','root');
-        define('DB_PASSWORD','1123581321');
+        define('DB_USER','user');
+        define('DB_PASSWORD','password');
         define('DB_NAME','taskman');
     } else {
 
         define('LOCALHOST',false);
         define('DEVELOPMENT_ENVIRONMENT',false);
         define('PRODUCTION_ENVIRONMENT',true);
-        define('DB_USER','islavisu');
-        define('DB_PASSWORD','1123581321');
+        define('DB_USER','user_pro');
+        define('DB_PASSWORD','password_pro');
         define('DB_NAME','evidaliahost_islavisual');
     }
 ?>
